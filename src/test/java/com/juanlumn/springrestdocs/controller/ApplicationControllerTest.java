3e13cc6928ec48ab9cdc407f67e0c2c530a6065b
juanlumn <juanlumn@gmail.com>
2018-01-09 20:30:32 +0100

    Added missing files

---
+++ src/test/java/com/juanlumn/springrestdocs/controller/ApplicationControllerTest.java
@@ -0,0 +1,39 @@
+package com.juanlumn.springrestdocs.controller;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ApplicationController.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
+public class ApplicationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+            .andExpect(content().string(containsString("Hello World"))).andDo(document("home"));
+    }
+
+    @Test
+    public void shouldReturnDefaultMessage2() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(document("home",
+            responseFields(fieldWithPath("message").description("The welcome message for the user."))));
+    }
+}
